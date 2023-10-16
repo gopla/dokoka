374bda9fe13e7aa0fb0ed5fab013ebe0eb3bb9a2
gopla <yudhistn@gmail.com>
2023-10-16 16:45:54 +0700

    fix: cluster marker, custom marker, fix useeffect

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import marker from './images/marker.png'
+
+const assets = {
+  images:{
+    marker,
+  },
+}
+
+export default assets
--- src/components/maps/MapComponent.jsx
+++ src/components/maps/MapComponent.jsx
@@ -2,6 +2,9 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useEffect, useState } from 'react'
 import { Stack } from '@mui/material'
 import { useOutletContext } from 'react-router-dom'
+import { Icon, divIcon, point } from 'leaflet'
+import assets from '../../assets'
+import MarkerClusterGroup from 'react-leaflet-cluster'
 
 const MapComponent = () => {
 	const [markers, setMarkers] = useState([])
@@ -9,25 +12,44 @@ const MapComponent = () => {
 
 	const locationsData = Object.values(locations) ?? []
 
-	useEffect(() => {
-		locationsData?.map((location) => {
-			setMarkers((prev) => [
-				...prev,
-				{
-					geocode: location.geocode,
-					popUp: (
-						<div>
-							<h3>{location.name}</h3>
-							<p>{location.description}</p>
-							<a href={location.googleMapsLink} target='_blank'>
-								Open in Google Maps
-							</a>
-						</div>
-					),
-				},
-			])
+  useEffect(() => {
+    // Clear the markers array before populating it
+    setMarkers([]);
+
+    locationsData?.map((location) => {
+      setMarkers((prev) => [
+        ...prev,
+        {
+          geocode: location.geocode,
+          popUp: (
+            <div>
+              <h3>{location.name}</h3>
+              <p>{location.description}</p>
+              <a href={location.googleMapsLink} target='_blank'>
+                Open in Google Maps
+              </a>
+            </div>
+          ),
+        },
+      ])
+    });
+  }, [locations]);
+
+
+	const customIcon = new Icon({
+		iconUrl: assets.images.marker,
+		iconSize: [38, 38],
+	})
+
+  console.log(markers);
+
+	const createClusterCustomIcon = function (cluster) {
+		return new divIcon({
+			html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
+			className: 'custom-marker-cluster',
+			iconSize: point(33, 33, true),
 		})
-	}, [locations])
+	}
 
 	return (
 		<Stack
@@ -36,16 +58,18 @@ const MapComponent = () => {
 				alignItems: 'center',
 			}}
 		>
-			<MapContainer center={[-7.983908, 112.621391]} zoom={10}>
+			<MapContainer center={[-7.983908, 112.621391]} zoom={12}>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 				/>
-				{markers.map((marker, index) => (
-					<Marker key={index} position={marker.geocode}>
-						<Popup>{marker.popUp}</Popup>
-					</Marker>
-				))}
+				<MarkerClusterGroup chunkedLoading iconCreateFunction={createClusterCustomIcon}>
+					{markers.map((marker, index) => (
+						<Marker key={index} position={marker.geocode} icon={customIcon}>
+							<Popup>{marker.popUp}</Popup>
+						</Marker>
+					))}
+				</MarkerClusterGroup>
 			</MapContainer>
 		</Stack>
 	)
